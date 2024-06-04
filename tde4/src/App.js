160762d6c347c35327332c68354c6d65d5666b68
OsmOVB <121311745+OsmOVB@users.noreply.github.com>
2024-06-04 20:47:21 -0300

    add tde5
    add task, remove task

--- tde4/src/App.js
+++ tde4/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
-import Card from "./components/Card";
+import ToDoList from "./pages/ToDoList";
 
 function App() {
   return (
-    <div style={{display: "flex" , justifyContent: "center", alignItems: "center"}}>
-      
-      <Card imagem={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7Iw-TYh9PmR9ZXc0he05F52ERMuQTX6iUtA&s"} titulo={"Card title"} texto={"Secondary text"}/>
+    <div
+
+    >
+      <ToDoList pageTitle="Lista de Tarefas" />
     </div>
   );
 }
---
+++ tde4/src/pages/ToDoList.js
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import "./ToDoList.css";
+import React from "react";
+
+const ListItem = ({ toDoItem, onDelete, OnDone }) => {
+  return (
+    <div
+      className="listItem"
+      style={toDoItem.done ? { backgroundColor: "#b6ebb5" } : null}
+      onClick={OnDone}
+    >
+    { toDoItem.id } - {toDoItem.name}
+      <button
+        className="addButton"
+        onClick={(e) => {
+          e.stopPropagation();
+          onDelete();
+        }}
+      >
+        Delete
+      </button>
+    </div>
+  );
+};
+
+function ToDoList({ pageTitle }) {
+  const [toDoList, setToDoList] = useState([]);
+  const [inputValue, setInputValue] = useState();
+  const [id, setId] = useState(0);
+
+  const onInputChange = (e) => {
+    console.log(e.target.value);
+    setInputValue(e.target.value);
+  };
+
+  const onClickAddTask = () => {
+    if (!inputValue) return;
+    setId(id + 1);
+    setToDoList([...toDoList, { id, name: inputValue, done: false }]);
+    setInputValue("");
+  };
+
+  const onClickDone = (id) => {
+    setToDoList((prevToDoList) =>
+      prevToDoList.map((item) =>
+        item.id === id ? { ...item, done: !item.done } : item
+      )
+    );
+  };
+
+  return (
+    <div>
+      <div className="header"></div>
+      <h1>{pageTitle}</h1>
+      <div className="inputContainer">Minha lista de tarefas</div>
+      <label for="taskText" className="inputLabel">
+        Digite a sua tarefa:
+      </label>
+      <input
+        id="taskText"
+        name="taskText"
+        className="input"
+        onChange={onInputChange}
+        value={inputValue}
+      />
+      <button className="addButton" onClick={onClickAddTask}>
+        +
+      </button>
+      <div className="list">
+        {toDoList.map((toDoItem) => (
+          <ListItem 
+          toDoItem={toDoItem}
+          onDelete={() => setToDoList(toDoList.filter((item) => item.id !== toDoItem.id))}
+          OnDone={() => onClickDone(toDoItem.id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ToDoList;
